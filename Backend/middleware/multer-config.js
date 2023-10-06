0da6aa5e509354a2bf337c516305e5e04a88faf7
mgdlnjp <magdeleinejpierre@gmail.com>
2023-10-07 00:27:12 +0200

    New commit

--- Backend/middleware/multer-config.js
+++ Backend/middleware/multer-config.js
@@ -1,19 +1,20 @@
 const multer = require("multer");
 
 const MIME_TYPES = {
+  "image/jpeg": "jpeg",
   "image/jpg": "jpg",
-  "image/jpeg": "jpg",
   "image/png": "png",
 };
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, "images");//destination du dossier d'enregistrement
+    callback(null, "images"); //destination du dossier d'enregistrement
   },
   filename: (req, file, callback) => {
-    const extension = MIME_TYPES[file.mimetype];
-    const name = file.originalname.split("." + extension).join("_");//methode split pour retirer le ".jpg" et rajouter le callback
-    callback(null, name + Date.now() + "." + extension);//personnalisation du nom du fichier par la date de création
+    const extension = MIME_TYPES[file.mimetype]; //extension souhaitée pour l'image finale
+    const originalname = file.originalname;
+    const sansExtension = originalname.split(".").slice(0, -1).join("."); //suppression de l'extension
+    callback(null, sansExtension + "_" + Date.now() + "." + extension); //personnalisation du nom du fichier par la date de création
   },
 });
 
